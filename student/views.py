4b8c146b2ee5c89f760187794c063e21c4225a7a
mohdnaim3258 <mohdnaim3258@gmail.com>
2018-01-03 12:21:01 +0800

    updated remove

--- student/views.py
+++ student/views.py
@@ -54,6 +54,19 @@ def student_edit(request,pk):
     return render(request, 'student/student_edit.html', {'form': form})
 
 
+def student_remove(request,pk):
+
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == "POST":
+        if request.POST.get("submit_yes", ""):
+            icnum = student.icnum
+            student.delete()
+            messages.success(request, "Student record with ID: " + str(icnum) + " has been removed! ")
+            return redirect(reverse_lazy('student_home'))
+
+    return render(request, 'student/student_confirm_delete.html', {'student': student, 'pk':pk})
+
+
 def student_detail(request,pk):
     student = get_object_or_404(Student, pk=pk)
     return render(request, 'student/student_detail.html', {'student': student})
