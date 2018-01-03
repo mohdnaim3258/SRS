a1adb2b2c02007dc3e924e1d6a8e10b82c2625b6
mohdnaim3258 <mohdnaim3258@gmail.com>
2018-01-03 12:11:59 +0800

    update student_edit

--- student/urls.py
+++ student/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^home/$',views.home_json,name='student_home_json'),
     url(r'^new/$', views.student_new, name='student_new'),
     url(r'^(?P<pk>\d+)/$', views.student_detail, name='student_detail'),
+    url(r'^(?P<pk>\d+)/edit$', views.student_edit, name='student_edit'),
     url(r'^list_json/$',(views.student_list_json.as_view()), name="student_list_json"),
 ]
